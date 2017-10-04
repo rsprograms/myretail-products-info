4cfb1be5c4ef3d97632acae6666baf9a30043006
rsprograms <rsanghvi@outlook.com>
2017-10-04 02:15:23 -0400

    Product Info Project
    Product Info Project

--- src/main/java/com/myRetail/MyRetailProductsInfoApplication.java
+++ src/main/java/com/myRetail/MyRetailProductsInfoApplication.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 import com.myRetail.entity.ProductInfoEntity;
-import com.myRetail.repository.ProductInfoRepository;
 import com.myRetail.service.ProductInfoService;
 import com.myRetail.utility.ProductInfoUtility;
 
